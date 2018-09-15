0d059a2831d0d44892b1da8f5557332b241fec76
Tyler Shake <tyler.shake@raytheon.com>
2018-09-15 11:31:45 -0400

    trying more html editing

--- html/doxy-boot.js
+++ html/doxy-boot.js
@@ -1,3 +1,6 @@
 $( document ).ready(function() {
-    $("p:first").replaceWith("Hello world!");
+    $("#top").remove();
+    $("#doc-content").addClass("main-panel");
+    $("#doc-content").removeAttr("style");
+    $("div.header").before("<nav class='navbar navbar-default'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='#'>Dashboard</a></div></div></nav>")
 });
