bcafa5bb38c758b37742505da889973d9b528e26
Tyler Shake <tyler.shake@raytheon.com>
2018-09-16 12:08:21 -0400

    changed html header

--- html/doxy-boot.js
+++ html/doxy-boot.js
@@ -1,6 +1,4 @@
 $( document ).ready(function() {
-    $("#top").remove();
     $("#doc-content").addClass("main-panel");
     $("#doc-content").removeAttr("style");
-    $("div.header").before("<nav class='navbar navbar-default'><div class='container-fluid'><div class='navbar-header'><a class='navbar-brand' href='#'>Dashboard</a></div></div></nav>")
 });
