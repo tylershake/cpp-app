47413a72861c9951f6cb24ad56ec278a615049ef
tshake <tshake@elder>
2018-09-16 15:36:53 -0400

    moved footer and content all to main page

--- html/doxy-boot.js
+++ html/doxy-boot.js
@@ -3,4 +3,12 @@ $( document ).ready(function() {
     $("#doc-content").removeAttr("style");
     $("#doc-content").prepend($("#top").clone());
     $("#top:first").remove();
+    $("div.header").wrapAll("<div class='content'></div>").wrapAll("<div class='container-fluid'></div>").wrapAll("<div class='row'></div>").wrapAll("<div class='col-md-12'></div>").wrapAll("<div class='card'></div>");
+    $("div.card").append($("div.contents").clone());
+    $("div.contents").eq(1).remove();
+    $("div.textblock").contents().unwrap();
+    $("div.contents").addClass("content").removeClass("contents");
+    $("div.headertitle").contents().unwrap();
+    $("div.title").contents().unwrap().wrapAll("<h4 class='title'></div>");
+    $("#doc-content").append("<footer class='footer'><div class='container-fluid'><nav class='pull-left'><ul><li><a href='http://localhost'>Homepage</a></li><li><a href='http://localhost'>Blog</a></li><li><a href='http://localhost'>Licenses</a></li></ul></nav><div class='copyright pull-right'>&copy; 2018 <a href='https://www.raytheon.com/ourcompany/bbn'>Raytheon BBN Technologies</a></div></div></footer>");
 });
