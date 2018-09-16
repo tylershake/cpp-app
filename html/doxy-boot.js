e6f56d4b5f60766f3ebdfc32194b065da741c3d3
tshake <tshake@elder>
2018-09-16 12:24:51 -0400

    moved top section to content section

--- html/doxy-boot.js
+++ html/doxy-boot.js
@@ -1,4 +1,6 @@
 $( document ).ready(function() {
     $("#doc-content").addClass("main-panel");
     $("#doc-content").removeAttr("style");
+    $("#doc-content").prepend($("#top").clone());
+    $("#top:first").remove();
 });
